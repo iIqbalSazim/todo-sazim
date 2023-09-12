ba26893a8210ae91280946a8ca411bc49afa6c96
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-12 20:49:21 +0600

    Refactor ActionButtons.jsx from class component to functional component.

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,34 +1,34 @@
 import { Button, Group, Text } from "@mantine/core";
 import { IconPlus, IconTrashX } from "@tabler/icons-react";
-import React from "react";
 import FilterByPriority from "./FilterByPriority";
 
-class ActionButtons extends React.Component {
-  render() {
-    const { handleNewToDoClick, setIsConfirmModalOpen, setFilter, filter } =
-      this.props;
-    return (
-      <>
-        <Group position="apart" p={"lg"}>
-          <Button
-            color="indigo"
-            leftIcon={<IconPlus size="1.125rem" />}
-            onClick={() => handleNewToDoClick()}
-          >
-            <Text>Add todo</Text>
-          </Button>
-          <FilterByPriority setFilter={setFilter} filter={filter} />
-          <Button
-            leftIcon={<IconTrashX size="1.125rem" />}
-            color="red"
-            onClick={() => setIsConfirmModalOpen()}
-          >
-            <Text>Remove Completed</Text>
-          </Button>
-        </Group>
-      </>
-    );
-  }
-}
+const ActionButtons = ({
+  handleNewToDoClick,
+  setIsConfirmModalOpen,
+  setFilter,
+  filter,
+}) => {
+  return (
+    <>
+      <Group position="apart" p={"lg"}>
+        <Button
+          color="indigo"
+          leftIcon={<IconPlus size="1.125rem" />}
+          onClick={() => handleNewToDoClick()}
+        >
+          <Text>Add todo</Text>
+        </Button>
+        <FilterByPriority setFilter={setFilter} filter={filter} />
+        <Button
+          leftIcon={<IconTrashX size="1.125rem" />}
+          color="red"
+          onClick={() => setIsConfirmModalOpen()}
+        >
+          <Text>Remove Completed</Text>
+        </Button>
+      </Group>
+    </>
+  );
+};
 
 export default ActionButtons;
