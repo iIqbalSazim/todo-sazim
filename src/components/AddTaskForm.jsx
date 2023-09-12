0f3f688c23391dfe80d6661e78f8e22324bc5ed6
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 02:22:54 +0600

    Refactor AddTaskForm.jsx from class component to functional component.
    - Refactor class component to functional component
    - Add new file formData.js to store selectPriorityData for use in AddTaskForm

--- src/components/AddTaskForm.jsx
+++ src/components/AddTaskForm.jsx
@@ -8,27 +8,26 @@ import {
   Title,
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
-import React from "react";
-import { HIGH, LOW, MEDIUM } from "../constants/priority";
+import { useState } from "react";
+import { LOW } from "../constants/priority";
 import { formatDueDate, generateCurrentTimeAndDate } from "../helper/helper";
 import { BUTTON } from "../constants/colors";
+import { selectPriorityData } from "../constants/formData";
 
-class AddTaskForm extends React.Component {
-  state = {
+const AddTaskForm = ({ createNewTask, closeForm }) => {
+  const [newTask, setNewTask] = useState({
     id: Math.round(Math.random() * 1000000),
     description: "Task details not provided",
     priority: LOW,
     isCompleted: false,
     createdAt: "",
     dueDate: new Date().toDateString().slice(0, 15),
-  };
+  });
 
-  handleSubmit = (e) => {
-    const { createNewTask, closeForm } = this.props;
+  const handleSubmit = (e) => {
     e.preventDefault();
-    createNewTask(this.state);
-
-    this.setState({
+    createNewTask(newTask);
+    setNewTask({
       description: "",
       priority: "",
       isCompleted: false,
@@ -36,63 +35,60 @@ class AddTaskForm extends React.Component {
       updatedAt: "",
       dueDate: "",
     });
+
     closeForm();
   };
 
-  setCreatedAt = () => {
+  const setCreatedAt = () => {
     let date = new Date();
     let createdAt = generateCurrentTimeAndDate(date);
-    this.setState({ createdAt: createdAt });
+    setNewTask({ ...newTask, createdAt: createdAt });
   };
 
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <Paper
-          shadow="lg"
-          p="xl"
-          radius={"md"}
-          my={20}
-          mx={"30%"}
-          bg={"white.0"}
-          withBorder
-        >
-          <SimpleGrid cols={1} verticalSpacing={"xl"}>
-            <Title ta={"center"}>Create Todo</Title>
-            <Textarea
-              placeholder="Write your task"
-              onChange={(e) => this.setState({ description: e.target.value })}
-            />
-            <Select
-              placeholder="Set priority"
-              defaultValue={"low"}
-              data={[
-                { value: HIGH, label: "High" },
-                { value: MEDIUM, label: "Medium" },
-                { value: LOW, label: "Low" },
-              ]}
-              onSelect={(e) =>
-                this.setState({ priority: e.target.value.toLowerCase() })
-              }
-            />
-            <DateInput
-              minDate={new Date()}
-              defaultValue={new Date(this.state.dueDate)}
-              onChange={(input) => {
-                this.setState({ dueDate: formatDueDate(input) });
-              }}
-              label="Date input"
-            />
-            <Center>
-              <Button color={BUTTON} type="submit" onClick={this.setCreatedAt}>
-                Create
-              </Button>
-            </Center>
-          </SimpleGrid>
-        </Paper>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <Paper
+        shadow="lg"
+        p="xl"
+        radius={"md"}
+        my={20}
+        mx={"30%"}
+        bg={"white.0"}
+        withBorder
+      >
+        <SimpleGrid cols={1} verticalSpacing={"xl"}>
+          <Title ta={"center"}>Create Todo</Title>
+          <Textarea
+            placeholder="Write your task"
+            onChange={(e) =>
+              setNewTask({ ...newTask, description: e.target.value })
+            }
+          />
+          <Select
+            placeholder="Set priority"
+            defaultValue={LOW}
+            data={selectPriorityData}
+            onSelect={(e) =>
+              setNewTask({ ...newTask, priority: e.target.value.toLowerCase() })
+            }
+          />
+          <DateInput
+            minDate={new Date()}
+            defaultValue={new Date(newTask.dueDate)}
+            onChange={(input) => {
+              setNewTask({ ...newTask, dueDate: formatDueDate(input) });
+            }}
+            label="Date input"
+          />
+          <Center>
+            <Button color={BUTTON} type="submit" onClick={setCreatedAt}>
+              Create
+            </Button>
+          </Center>
+        </SimpleGrid>
+      </Paper>
+    </form>
+  );
+};
 
 export default AddTaskForm;
---
+++ src/constants/formData.js
@@ -0,0 +1,7 @@
+import { HIGH, MEDIUM, LOW } from "./priority";
+
+export const selectPriorityData = [
+  { value: HIGH, label: "High" },
+  { value: MEDIUM, label: "Medium" },
+  { value: LOW, label: "Low" },
+];
