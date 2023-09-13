16ec1584ad1967ce953bb27f349fd78433da3ccb
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 14:59:48 +0600

    Refactor ActiveTask.jsx from class component to functional component.

--- src/components/ActiveTask.jsx
+++ src/components/ActiveTask.jsx
@@ -4,69 +4,65 @@ import {
   IconCircleMinus,
   IconEditCircle,
 } from "@tabler/icons-react";
-import React from "react";
 
-class ActiveTask extends React.Component {
-  render() {
-    const {
-      task,
-      assignColorByPriority,
-      handleToggleIsCompleted,
-      openEditForm,
-      handleDelete,
-    } = this.props;
-    return (
-      <Card
-        bg={assignColorByPriority(task.priority)}
-        shadow={"md"}
-        radius={"md"}
-        px="xl"
-        py="xs"
-        mx="xl"
-        withBorder
-      >
-        <Group position="apart">
-          <Text weight={500} size={"15px"} maw={"70%"}>
-            {task.description}
-          </Text>
-          <Group position="right">
-            <Badge color={assignColorByPriority(task.priority)} variant="light">
-              {task.priority}
-            </Badge>
-            <ActionIcon
-              variant="light"
-              onClick={() => handleToggleIsCompleted(task.id)}
-              bg="teal.1"
-            >
-              <IconCircleCheck size="1.125rem" />
-            </ActionIcon>
-            <ActionIcon
-              variant="light"
-              onClick={() => openEditForm(task)}
-              bg="white"
-            >
-              <IconEditCircle size="1.125rem" />
-            </ActionIcon>
-            <ActionIcon
-              variant="light"
-              bg="red.1"
-              onClick={() => handleDelete(task.id)}
-            >
-              <IconCircleMinus size="1.125rem" />
-            </ActionIcon>
-          </Group>
+const ActiveTask = ({
+  task,
+  assignColorByPriority,
+  handleToggleIsCompleted,
+  openEditForm,
+  handleDelete,
+}) => {
+  return (
+    <Card
+      bg={assignColorByPriority(task.priority)}
+      shadow={"md"}
+      radius={"md"}
+      px="xl"
+      py="xs"
+      mx="xl"
+      withBorder
+    >
+      <Group position="apart">
+        <Text weight={500} size={"15px"} maw={"70%"}>
+          {task.description}
+        </Text>
+        <Group position="right">
+          <Badge color={assignColorByPriority(task.priority)} variant="light">
+            {task.priority}
+          </Badge>
+          <ActionIcon
+            variant="light"
+            onClick={() => handleToggleIsCompleted(task.id)}
+            bg="teal.1"
+          >
+            <IconCircleCheck size="1.125rem" />
+          </ActionIcon>
+          <ActionIcon
+            variant="light"
+            onClick={() => openEditForm(task)}
+            bg="white"
+          >
+            <IconEditCircle size="1.125rem" />
+          </ActionIcon>
+          <ActionIcon
+            variant="light"
+            bg="red.1"
+            onClick={() => handleDelete(task.id)}
+          >
+            <IconCircleMinus size="1.125rem" />
+          </ActionIcon>
         </Group>
-        <Group position="apart" pt={"md"}>
-          <Text size="xs" weight={300}>
-            Created at {task.createdAt}
-          </Text>
-          <Code size={"sm"} radius={"md"} color="red">
-            Complete by: {task.dueDate}
-          </Code>
-        </Group>
-      </Card>
-    );
-  }
-}
+      </Group>
+      <Group position="apart" pt={"md"}>
+        <Text size="xs" weight={300}>
+          Created at {task.createdAt}
+        </Text>
+        <Code size={"sm"} radius={"md"} color="red">
+          Complete by: {task.dueDate}
+        </Code>
+      </Group>
+    </Card>
+  );
+};
 
 export default ActiveTask;
