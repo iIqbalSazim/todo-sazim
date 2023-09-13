aec95df93d74f760985cb1f48981b98d285fe542
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 15:56:52 +0600

    Refactor FilterByPriority.jsx from class component to functional component.
    - Remove unnecessary oneliner methods

--- src/components/FilterByPriority.jsx
+++ src/components/FilterByPriority.jsx
@@ -1,66 +1,51 @@
-import React from "react";
-import { Button, Group } from "@mantine/core";
+import { Button } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
 import { HIGH, LOW, MEDIUM } from "../constants/priority";
-import { BUTTON, PRIORITYBUTTON } from "../constants/colors";
+import { PRIORITYBUTTON } from "../constants/colors";
 
-class FilterByPriority extends React.Component {
-  handleClearFilterByPriority = () => {
-    const { setFilter } = this.props;
-    setFilter("");
-  };
-
-  handleButtonClick = (value) => {
-    this.props.setFilter(value);
-  };
-
-  render() {
-    const { filter } = this.props;
-    return (
-      <>
-        <Button.Group>
-          <Button
-            variant={
-              filter.priority === HIGH && !filter.dueDate ? "filled" : "outline"
-            }
-            color={PRIORITYBUTTON}
-            onClick={() => this.handleButtonClick(HIGH)}
-          >
-            High
-          </Button>
-          <Button
-            variant={
-              filter.priority === MEDIUM && !filter.dueDate
-                ? "filled"
-                : "outline"
-            }
-            color={PRIORITYBUTTON}
-            onClick={() => this.handleButtonClick(MEDIUM)}
-          >
-            Medium
-          </Button>
-          <Button
-            variant={
-              filter.priority === LOW && !filter.dueDate ? "filled" : "outline"
-            }
-            color={PRIORITYBUTTON}
-            onClick={() => this.handleButtonClick(LOW)}
-          >
-            Low
-          </Button>
-          <Button
-            variant="light"
-            color={"indigo.4"}
-            size="sm"
-            rightIcon={<IconX size={"1rem"} />}
-            onClick={() => this.handleClearFilterByPriority()}
-          >
-            Clear Filter
-          </Button>
-        </Button.Group>
-      </>
-    );
-  }
-}
+const FilterByPriority = ({ setFilter, filter }) => {
+  return (
+    <>
+      <Button.Group>
+        <Button
+          variant={
+            filter.priority === HIGH && !filter.dueDate ? "filled" : "outline"
+          }
+          color={PRIORITYBUTTON}
+          onClick={() => setFilter(HIGH)}
+        >
+          High
+        </Button>
+        <Button
+          variant={
+            filter.priority === MEDIUM && !filter.dueDate ? "filled" : "outline"
+          }
+          color={PRIORITYBUTTON}
+          onClick={() => setFilter(MEDIUM)}
+        >
+          Medium
+        </Button>
+        <Button
+          variant={
+            filter.priority === LOW && !filter.dueDate ? "filled" : "outline"
+          }
+          color={PRIORITYBUTTON}
+          onClick={() => setFilter(LOW)}
+        >
+          Low
+        </Button>
+        <Button
+          variant="light"
+          color={"indigo.4"}
+          size="sm"
+          rightIcon={<IconX size={"1rem"} />}
+          onClick={() => setFilter("")}
+        >
+          Clear Filter
+        </Button>
+      </Button.Group>
+    </>
+  );
+};
 
 export default FilterByPriority;
