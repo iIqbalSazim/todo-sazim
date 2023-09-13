0e2cfc8bcd14019edd093e6a4038840684bdf75d
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 19:03:21 +0600

    Refactor Home.jsx from class component to functional component.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
-import { AppShell, Header, Title, Navbar } from "@mantine/core";
-import React from "react";
+import { AppShell, Header, Title, Navbar, ThemeIcon } from "@mantine/core";
+import { useEffect, useState } from "react";
 import AddTaskForm from "../components/AddTaskForm";
 import TasksList from "../components/TasksList";
 import FilterByCompletedStatus from "../components/FilterByCompletedStatus";
@@ -12,233 +12,194 @@ import ConfirmModal from "../components/ConfirmModal";
 import FilterByDueDate from "../components/FilterByDueDate";
 import DisplayDate from "../components/DisplayDate";
 
-class Home extends React.Component {
-  state = {
-    tasks: [],
-    isAddFormOpen: false,
-    isEditFormOpen: false,
-    isConfirmModalOpen: false,
-    toBeEdited: {},
-    filter: { status: "all", priority: "", dueDate: false },
-    trash: [],
+const Home = () => {
+  const [tasks, setTasks] = useState([]);
+  const [isAddFormOpen, setIsAddFormOpen] = useState(false);
+  const [isEditFormOpen, setIsEditFormOpen] = useState(false);
+  const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [toBeEdited, setToBeEdited] = useState({});
+  const [filter, setFilter] = useState({
+    status: "all",
+    priority: "",
+    dueDate: false,
+  });
+  const [trash, setTrash] = useState([]);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem("tasks")) || []);
+    setTrash(JSON.parse(localStorage.getItem("trash")) || []);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("trash", JSON.stringify(trash));
+  }, [trash]);
+
+  const setIsConfirmModalToOpen = () => {
+    setIsConfirmModalOpen(true);
   };
 
-  setIsConfirmModalOpen = () => {
-    this.setState((prevState) => {
-      return { isConfirmModalOpen: true };
-    });
+  const setIsConfirmModalToClose = () => {
+    setIsConfirmModalOpen(false);
   };
 
-  setIsConfirmModalClose = () => {
-    this.setState((prevState) => {
-      return { isConfirmModalOpen: false };
-    });
+  const toggleFilterDueDate = () => {
+    setFilter({ ...filter, dueDate: !filter.dueDate });
   };
 
-  toggleFilterDueDate = () => {
-    this.setState((prevState) => {
-      return {
-        filter: { ...prevState.filter, dueDate: !prevState.filter.dueDate },
-      };
-    });
+  const setCompletedStatusFilter = (status) => {
+    setFilter({ ...filter, status: status });
   };
 
-  setCompletedStatusFilter = (status) => {
-    this.setState((prevState) => {
-      return { filter: { ...prevState.filter, status: status } };
-    });
+  const setPriorityStatusFilter = (priority) => {
+    setFilter({ ...filter, priority: priority });
   };
 
-  setPriorityStatusFilter = (priority) => {
-    this.setState((prevState) => {
-      return { filter: { ...prevState.filter, priority: priority } };
-    });
-  };
-
-  setIsCompleted = (id) => {
-    const updatedTasks = this.state.tasks.map((task) => {
+  const setIsCompleted = (id) => {
+    const updatedTasks = tasks.map((task) => {
       if (task.id === id) {
         return { ...task, isCompleted: !task.isCompleted };
       }
       return task;
     });
-
-    this.setState({ tasks: updatedTasks });
+    setTasks(updatedTasks);
   };
 
-  clearAllCompletedTasks = () => {
-    let updatedList = this.state.tasks.filter(
-      (task) => task.isCompleted !== true
-    );
+  const clearAllCompletedTasks = () => {
+    let updatedList = tasks.filter((task) => task.isCompleted !== true);
 
-    this.setState((prevState) => {
-      if (JSON.stringify(prevState.tasks) === JSON.stringify(updatedList)) {
-        alert("You have no completed tasks to remove");
-      } else {
-        return { tasks: updatedList };
-      }
-    });
+    if (JSON.stringify(tasks) === JSON.stringify(updatedList)) {
+      alert("You have no completed tasks to remove");
+    } else {
+      setTasks(updatedList);
+    }
   };
 
-  setIsAddFormOpen = () => {
-    this.setState((prevState) => {
-      return { isAddFormOpen: !prevState.isAddFormOpen };
-    });
+  const toggleIsAddFormOpen = () => {
+    setIsAddFormOpen(!isAddFormOpen);
   };
 
-  addNewTask = (task) => {
-    this.setState((prevState) => {
-      return { tasks: [...prevState.tasks, task] };
-    });
+  const addNewTask = (task) => {
+    setTasks([...tasks, task]);
   };
 
-  setIsEditFormOpen = () => {
-    this.setState({
-      isEditFormOpen: true,
-    });
+  const setIsEditFormToOpen = () => {
+    setIsEditFormOpen(true);
   };
 
-  setIsEditFormClose = () => {
-    this.setState({
-      isEditFormOpen: false,
-    });
+  const setIsEditFormToClose = () => {
+    setIsEditFormOpen(false);
   };
 
-  handleOpenEditForm = (task) => {
-    this.setIsEditFormOpen();
-    this.setState({ toBeEdited: task });
+  const handleOpenEditForm = (task) => {
+    setIsEditFormToOpen();
+    setToBeEdited(task);
   };
 
-  editTask = (edited) => {
-    const index = findIndexWithId(this.state.tasks, edited.id);
-    const updatedTasks = this.state.tasks;
+  const editTask = (edited) => {
+    const index = findIndexWithId(tasks, edited.id);
+    const updatedTasks = tasks;
     updatedTasks[index] = edited;
-
-    this.setState({ tasks: updatedTasks });
+    setTasks(updatedTasks);
   };
 
-  deleteTask = (id) => {
-    let tasks = this.state.tasks;
+  const deleteTask = (id) => {
     let removedTask = tasks.filter((task) => task.id === id)[0];
-    this.addToTrash(removedTask);
+    addToTrash(removedTask);
     let updatedList = tasks.filter((task) => task.id !== id);
-    this.setState({ tasks: updatedList });
+    setTasks(updatedList);
   };
 
-  addToTrash = (removedTask) => {
-    this.setState((prevState) => {
-      return { trash: [...prevState.trash, removedTask] };
-    });
+  const addToTrash = (removedTask) => {
+    setTrash([...trash, removedTask]);
   };
 
-  emptyTrash = () => {
-    this.setState({ trash: [] });
+  const emptyTrash = () => {
+    setTrash([]);
     localStorage.removeItem("trash");
   };
 
-  retrieveAll = () => {
-    this.setState((prevState) => {
-      return { tasks: [...prevState.tasks, ...this.state.trash] };
-    });
-    this.emptyTrash();
+  const retrieveAll = () => {
+    setTasks([...tasks, ...trash]);
+    emptyTrash();
   };
 
-  componentDidMount() {
-    this.setState({
-      tasks: JSON.parse(localStorage.getItem("tasks")) || [],
-      trash: JSON.parse(localStorage.getItem("trash")) || [],
-    });
-  }
-
-  componentDidUpdate() {
-    let local = JSON.parse(localStorage.getItem("trash")) || [];
-
-    if (JSON.stringify(this.state.trash) !== JSON.stringify(local)) {
-      localStorage.setItem("trash", JSON.stringify(this.state.trash));
-    }
-  }
-
-  render() {
-    return (
-      <>
-        <AppShell
-          padding="xl"
-          navbar={
-            <Navbar width={{ base: 200 }} height={"full"} p="xl" bg={"gray.2"}>
-              <Navbar.Section grow mt={"120%"}>
-                <FilterByCompletedStatus
-                  setFilter={this.setCompletedStatusFilter}
-                />
-              </Navbar.Section>
-            </Navbar>
-          }
-          header={
-            <Header
-              p={"xl"}
-              height={"90"}
-              sx={{ boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)" }}
+  return (
+    <>
+      <AppShell
+        padding="xl"
+        navbar={
+          <Navbar width={{ base: 200 }} height={"full"} p="xl" bg={"gray.2"}>
+            <Navbar.Section grow mt={"120%"}>
+              <FilterByCompletedStatus setFilter={setCompletedStatusFilter} />
+            </Navbar.Section>
+          </Navbar>
+        }
+        header={
+          <Header
+            p={"xl"}
+            height={"90"}
+            sx={{ boxShadow: "0px 2px 4px rgba(0, 0, 0, 0.1)" }}
+          >
+            <Title
+              order={1}
+              ta={"left"}
+              h={"fit"}
+              color="indigo.9"
+              fw={"200"}
+              tt="capitalize"
             >
-              <Title
-                order={1}
-                ta={"left"}
-                h={"fit"}
-                color="indigo.9"
-                fw={"200"}
-                tt="capitalize"
-              >
-                &lt;TODO OR <strong>!</strong>TODO /&gt;
-              </Title>
-            </Header>
-          }
-        >
-          <DisplayDate />
-          <ActionButtons
-            filter={this.state.filter}
-            setFilter={this.setPriorityStatusFilter}
-            handleNewToDoClick={this.setIsAddFormOpen}
-            setIsConfirmModalOpen={this.setIsConfirmModalOpen}
+              &lt;TODO OR <strong>!</strong>TODO /&gt;
+            </Title>
+          </Header>
+        }
+      >
+        <DisplayDate />
+        <ActionButtons
+          filter={filter}
+          setFilter={setPriorityStatusFilter}
+          handleNewToDoClick={toggleIsAddFormOpen}
+          setIsConfirmModalOpen={setIsConfirmModalToOpen}
+        />
+        <FilterByDueDate
+          toggleFilterDueDate={toggleFilterDueDate}
+          filter={filter}
+        />
+        {isAddFormOpen ? (
+          <AddTaskForm
+            createNewTask={addNewTask}
+            closeForm={toggleIsAddFormOpen}
           />
-          <FilterByDueDate
-            toggleFilterDueDate={this.toggleFilterDueDate}
-            filter={this.state.filter}
+        ) : null}
+        {isEditFormOpen ? (
+          <EditForm
+            closeModal={setIsEditFormToClose}
+            isOpen={isEditFormOpen}
+            task={toBeEdited}
+            editTask={editTask}
           />
-          {this.state.isAddFormOpen ? (
-            <AddTaskForm
-              createNewTask={this.addNewTask}
-              closeForm={this.setIsAddFormOpen}
-            />
-          ) : null}
-          {this.state.isEditFormOpen ? (
-            <EditForm
-              closeModal={this.setIsEditFormClose}
-              isOpen={this.state.isEditFormOpen}
-              task={this.state.toBeEdited}
-              editTask={this.editTask}
-            />
-          ) : null}
-          {this.state.isConfirmModalOpen ? (
-            <ConfirmModal
-              isConfirmModalOpen={this.state.isConfirmModalOpen}
-              closeModal={this.setIsConfirmModalClose}
-              clearAllCompletedTasks={this.clearAllCompletedTasks}
-            />
-          ) : null}
-          <TasksList
-            tasks={this.state.tasks}
-            handleDelete={this.deleteTask}
-            openEditForm={this.handleOpenEditForm}
-            handleToggleIsCompleted={this.setIsCompleted}
-            filter={this.state.filter}
+        ) : null}
+        {isConfirmModalOpen ? (
+          <ConfirmModal
+            isConfirmModalOpen={isConfirmModalOpen}
+            closeModal={setIsConfirmModalToClose}
+            clearAllCompletedTasks={clearAllCompletedTasks}
           />
-          <Bin
-            handleRetrieveAllClick={this.retrieveAll}
-            handleEmptyBinClick={this.emptyTrash}
-            trash={this.state.trash}
-          />
-        </AppShell>
-      </>
-    );
-  }
-}
+        ) : null}
+        <TasksList
+          tasks={tasks}
+          handleDelete={deleteTask}
+          openEditForm={handleOpenEditForm}
+          handleToggleIsCompleted={setIsCompleted}
+          filter={filter}
+        />
+        <Bin
+          handleRetrieveAllClick={retrieveAll}
+          handleEmptyBinClick={emptyTrash}
+          trash={trash}
+        />
+      </AppShell>
+    </>
+  );
+};
 
 export default Home;
