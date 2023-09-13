7c63f69aa07ecb86fba31ae360607b36038dcee5
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 15:43:22 +0600

    Refactor FilterByDueDate.jsx from class component to functional component.

--- src/components/FilterByDueDate.jsx
+++ src/components/FilterByDueDate.jsx
@@ -1,27 +1,24 @@
 import { Button, Group } from "@mantine/core";
 import { IconChevronDown, IconX } from "@tabler/icons-react";
-import React from "react";
 
-class FilterByDueDate extends React.Component {
-  render() {
-    return (
-      <Group position="right" mx={"lg"} pt={"xl"}>
-        <Button
-          rightIcon={
-            !this.props.filter.dueDate ? (
-              <IconChevronDown size={"1rem"} />
-            ) : (
-              <IconX size={"1rem"} />
-            )
-          }
-          variant="outline"
-          onClick={() => this.props.toggleFilterDueDate()}
-        >
-          Sort by Due Date
-        </Button>
-      </Group>
-    );
-  }
-}
+const FilterByDueDate = ({ filter, toggleFilterDueDate }) => {
+  return (
+    <Group position="right" mx={"lg"} pt={"xl"}>
+      <Button
+        rightIcon={
+          !filter.dueDate ? (
+            <IconChevronDown size={"1rem"} />
+          ) : (
+            <IconX size={"1rem"} />
+          )
+        }
+        variant="outline"
+        onClick={() => toggleFilterDueDate()}
+      >
+        Sort by Due Date
+      </Button>
+    </Group>
+  );
+};
 
 export default FilterByDueDate;
