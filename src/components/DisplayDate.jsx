78c80ed28c92fe0525ebadabb416d94bb4833e02
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 15:22:21 +0600

    Refactor DisplayDate.jsx from class component to functional component.
    - Remove unnecessary backticks and dollar

--- src/components/DisplayDate.jsx
+++ src/components/DisplayDate.jsx
@@ -1,20 +1,14 @@
 import { Center, Title } from "@mantine/core";
-import React from "react";
 import { DISPLAYDATE } from "../constants/displayDate";
 
-class DisplayDate extends React.Component {
-  render() {
-    return (
-      <Center>
-        <Title
-          radius={"sm"}
-          mb={"xl"}
-          pt={"sm"}
-          color={DISPLAYDATE}
-        >{`${new Date().toDateString()}`}</Title>
-      </Center>
-    );
-  }
-}
+const DisplayDate = () => {
+  return (
+    <Center>
+      <Title radius={"sm"} mb={"xl"} pt={"sm"} color={DISPLAYDATE}>
+        {new Date().toDateString()}
+      </Title>
+    </Center>
+  );
+};
 
 export default DisplayDate;
