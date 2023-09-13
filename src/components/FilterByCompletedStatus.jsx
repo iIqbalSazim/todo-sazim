9713dbc3c8cffbcb370db64b0aa38b4d9d5f58a0
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 15:38:57 +0600

    Refactor FilterByCompletedStatus.jsx from class component to functional component.

--- src/components/FilterByCompletedStatus.jsx
+++ src/components/FilterByCompletedStatus.jsx
@@ -1,26 +1,23 @@
 import { Tabs } from "@mantine/core";
-import React from "react";
 import { BUTTON } from "../constants/colors";
 
-class FilterByCompletedStatus extends React.Component {
-  render() {
-    return (
-      <Tabs
-        orientation="vertical"
-        variant="pills"
-        color={BUTTON}
-        defaultValue="all"
-        onTabChange={(value) => this.props.setFilter(value)}
-        h={"180px"}
-      >
-        <Tabs.List position="apart">
-          <Tabs.Tab value="all">All</Tabs.Tab>
-          <Tabs.Tab value="active">Active</Tabs.Tab>
-          <Tabs.Tab value="completed">Completed</Tabs.Tab>
-        </Tabs.List>
-      </Tabs>
-    );
-  }
-}
+const FilterByCompletedStatus = ({ setFilter }) => {
+  return (
+    <Tabs
+      orientation="vertical"
+      variant="pills"
+      color={BUTTON}
+      defaultValue="all"
+      onTabChange={(value) => setFilter(value)}
+      h={"180px"}
+    >
+      <Tabs.List position="apart">
+        <Tabs.Tab value="all">All</Tabs.Tab>
+        <Tabs.Tab value="active">Active</Tabs.Tab>
+        <Tabs.Tab value="completed">Completed</Tabs.Tab>
+      </Tabs.List>
+    </Tabs>
+  );
+};
 
 export default FilterByCompletedStatus;
