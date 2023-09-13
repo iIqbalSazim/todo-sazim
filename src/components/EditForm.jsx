a3f374ab9e063a390346cab6869cec0813145d2c
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-13 15:33:09 +0600

    Refactor EditForm.jsx from class component to functional component.
    - Add new file formData.js to store selectPriorityData for use in EditTaskForm

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import {
   Button,
   Center,
@@ -9,63 +8,60 @@ import {
   Title,
 } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
-import { HIGH, LOW, MEDIUM } from "../constants/priority";
+import { selectPriorityData } from "../constants/formData";
 import { formatDueDate } from "../helper/helper";
 import { BUTTON } from "../constants/colors";
+import { useState } from "react";
 
-class EditForm extends React.Component {
-  state = this.props.task;
+const EditForm = ({ task, editTask, isOpen, closeModal }) => {
+  const [editedTask, setEditedTask] = useState({ ...task });
 
-  handleSubmit = (e) => {
-    const { editTask, closeModal } = this.props;
+  const handleSubmit = (e) => {
     e.preventDefault();
 
-    editTask(this.state);
+    editTask(editedTask);
     closeModal();
   };
 
-  render() {
-    const { isOpen, closeModal } = this.props;
-
-    return (
-      <Modal opened={isOpen} onClose={() => closeModal()} centered>
-        <form onSubmit={this.handleSubmit}>
-          <SimpleGrid cols={1} p={"lg"}>
-            <Title ta={"center"}>Edit Todo</Title>
-            <Textarea
-              defaultValue={this.state.description}
-              onChange={(e) => this.setState({ description: e.target.value })}
-            />
-            <Select
-              data={[
-                { value: HIGH, label: "High" },
-                { value: MEDIUM, label: "Medium" },
-                { value: LOW, label: "Low" },
-              ]}
-              defaultValue={this.state.priority}
-              onSelect={(e) =>
-                this.setState({ priority: e.target.value.toLowerCase() })
-              }
-            />
-            <DateInput
-              minDate={new Date()}
-              defaultValue={new Date(this.state.dueDate)}
-              onChange={(input) =>
-                this.setState({ dueDate: formatDueDate(input) })
-              }
-              label="Date input"
-              placeholder="Date input"
-            />
-            <Center>
-              <Button color={BUTTON} type="submit">
-                Update
-              </Button>
-            </Center>
-          </SimpleGrid>
-        </form>
-      </Modal>
-    );
-  }
-}
+  return (
+    <Modal opened={isOpen} onClose={() => closeModal()} centered>
+      <form onSubmit={handleSubmit}>
+        <SimpleGrid cols={1} p={"lg"}>
+          <Title ta={"center"}>Edit Todo</Title>
+          <Textarea
+            defaultValue={editedTask.description}
+            onChange={(e) =>
+              setEditedTask({ ...editedTask, description: e.target.value })
+            }
+          />
+          <Select
+            data={selectPriorityData}
+            defaultValue={editedTask.priority}
+            onSelect={(e) =>
+              setEditedTask({
+                ...editedTask,
+                priority: e.target.value.toLowerCase(),
+              })
+            }
+          />
+          <DateInput
+            minDate={new Date()}
+            defaultValue={new Date(editedTask.dueDate)}
+            onChange={(input) =>
+              setEditedTask({ ...editedTask, dueDate: formatDueDate(input) })
+            }
+            label="Date input"
+            placeholder="Date input"
+          />
+          <Center>
+            <Button color={BUTTON} type="submit">
+              Update
+            </Button>
+          </Center>
+        </SimpleGrid>
+      </form>
+    </Modal>
+  );
+};
 
 export default EditForm;
