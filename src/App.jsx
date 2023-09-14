989b29684e7026b4f67977ec4cc86c6434efc9cb
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-14 15:51:45 +0600

    Refactor App.jsx from class component to functional component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import React from "react";
 import { MantineProvider } from "@mantine/core";
 import Home from "./pages/home";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-          <Home />
-        </MantineProvider>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        <Home />
+      </MantineProvider>
+    </>
+  );
+};
+
+export default App;
