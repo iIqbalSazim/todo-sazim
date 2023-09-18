6f32f3fbbdbe2b34587fdca0fecc49c66e899aa9
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-19 03:44:43 +0600

    Make FilterByDueDate responsive

--- src/components/FilterByDueDate.jsx
+++ src/components/FilterByDueDate.jsx
@@ -1,22 +1,41 @@
-import { Button, Group } from "@mantine/core";
+import { Button, Group, MediaQuery, Text } from "@mantine/core";
 import { IconChevronDown, IconX } from "@tabler/icons-react";
 
 const FilterByDueDate = ({ filter, toggleFilterDueDate }) => {
   return (
-    <Group position="right" mx={"lg"} pt={"xl"}>
-      <Button
-        rightIcon={
-          !filter.dueDate ? (
-            <IconChevronDown size={"1rem"} />
-          ) : (
-            <IconX size={"1rem"} />
-          )
-        }
-        variant="outline"
-        onClick={() => toggleFilterDueDate()}
-      >
-        Sort by Due Date
-      </Button>
+    <Group position="right" mr={"md"} pt={"xl"}>
+      <MediaQuery largerThan={"md"} styles={{ display: "none" }}>
+        <Button
+          rightIcon={
+            !filter.dueDate ? (
+              <IconChevronDown size={"1rem"} />
+            ) : (
+              <IconX size={"1rem"} />
+            )
+          }
+          variant="outline"
+          size="xs"
+          onClick={() => toggleFilterDueDate()}
+        >
+          <Text size={"xs"}>Sort by Due Date</Text>
+        </Button>
+      </MediaQuery>
+
+      <MediaQuery smallerThan={"md"} styles={{ display: "none" }}>
+        <Button
+          rightIcon={
+            !filter.dueDate ? (
+              <IconChevronDown size={"1rem"} />
+            ) : (
+              <IconX size={"1rem"} />
+            )
+          }
+          variant="outline"
+          onClick={() => toggleFilterDueDate()}
+        >
+          Sort by Due Date
+        </Button>
+      </MediaQuery>
     </Group>
   );
 };
