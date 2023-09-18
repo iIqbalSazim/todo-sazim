57f21609ceca3ac21616a5f42b4a94d3587d4500
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-19 03:17:07 +0600

    ActionButtons & FilterByPriority responsive for sm and md screens

--- src/components/ActionButtons.jsx
+++ src/components/ActionButtons.jsx
@@ -1,4 +1,4 @@
-import { Button, Group, Text } from "@mantine/core";
+import { Button, Flex, Group, MediaQuery, Text, Title } from "@mantine/core";
 import { IconPlus, IconTrashX } from "@tabler/icons-react";
 import FilterByPriority from "./FilterByPriority";
 
@@ -10,23 +10,48 @@ const ActionButtons = ({
 }) => {
   return (
     <>
-      <Group position="apart" p={"lg"}>
-        <Button
-          color="indigo"
-          leftIcon={<IconPlus size="1.125rem" />}
-          onClick={() => handleNewToDoClick()}
-        >
-          <Text>Add todo</Text>
-        </Button>
-        <FilterByPriority setFilter={setFilter} filter={filter} />
-        <Button
-          leftIcon={<IconTrashX size="1.125rem" />}
-          color="red"
-          onClick={() => setIsConfirmModalOpen()}
-        >
-          <Text>Remove Completed</Text>
-        </Button>
-      </Group>
+      <MediaQuery smallerThan={"md"} styles={{ display: "none" }}>
+        <Group position="apart" p={"lg"}>
+          <Button
+            color="indigo"
+            leftIcon={<IconPlus size="1.125rem" />}
+            onClick={() => handleNewToDoClick()}
+          >
+            <Text>Add todo</Text>
+          </Button>
+          <FilterByPriority setFilter={setFilter} filter={filter} />
+          <Button
+            leftIcon={<IconTrashX size="1.125rem" />}
+            color="red"
+            onClick={() => setIsConfirmModalOpen()}
+          >
+            <Text>Remove Completed</Text>
+          </Button>
+        </Group>
+      </MediaQuery>
+      <MediaQuery largerThan={"md"} styles={{ display: "none" }}>
+        <Flex direction={"column"} align={"center"}>
+          <Group position="center" py={"xl"}>
+            <Button
+              size="xs"
+              color="indigo"
+              leftIcon={<IconPlus size="1rem" />}
+              onClick={() => handleNewToDoClick()}
+            >
+              <Text size={"xs"}>Todo</Text>
+            </Button>
+            <Button
+              size="xs"
+              leftIcon={<IconTrashX size="1rem" />}
+              color="red"
+              onClick={() => setIsConfirmModalOpen()}
+            >
+              <Text size={"xs"}>Completed</Text>
+            </Button>
+          </Group>
+          <FilterByPriority setFilter={setFilter} filter={filter} />
+        </Flex>
+      </MediaQuery>
     </>
   );
 };
--- src/components/FilterByPriority.jsx
+++ src/components/FilterByPriority.jsx
@@ -1,4 +1,4 @@
-import { Button } from "@mantine/core";
+import { Button, MediaQuery, Menu } from "@mantine/core";
 import { IconX } from "@tabler/icons-react";
 import { PRIORITY } from "../constants/constant";
 import { COLORS } from "../constants/constant";
@@ -6,50 +6,102 @@ import { COLORS } from "../constants/constant";
 const FilterByPriority = ({ setFilter, filter }) => {
   return (
     <>
-      <Button.Group>
-        <Button
-          variant={
-            filter.priority === PRIORITY.HIGH && !filter.dueDate
-              ? "filled"
-              : "outline"
-          }
-          color={COLORS.PRIORITYBUTTON}
-          onClick={() => setFilter(PRIORITY.HIGH)}
-        >
-          High
-        </Button>
-        <Button
-          variant={
-            filter.priority === PRIORITY.MEDIUM && !filter.dueDate
-              ? "filled"
-              : "outline"
-          }
-          color={COLORS.PRIORITYBUTTON}
-          onClick={() => setFilter(PRIORITY.MEDIUM)}
-        >
-          Medium
-        </Button>
-        <Button
-          variant={
-            filter.priority === PRIORITY.LOW && !filter.dueDate
-              ? "filled"
-              : "outline"
-          }
-          color={COLORS.PRIORITYBUTTON}
-          onClick={() => setFilter(PRIORITY.LOW)}
-        >
-          Low
-        </Button>
-        <Button
-          variant="light"
-          color={"indigo.4"}
-          size="sm"
-          rightIcon={<IconX size={"1rem"} />}
-          onClick={() => setFilter("")}
-        >
-          Clear Filter
-        </Button>
-      </Button.Group>
+      <MediaQuery smallerThan={"md"} styles={{ display: "none" }}>
+        <Button.Group>
+          <Button
+            variant={
+              filter.priority === PRIORITY.HIGH && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.HIGH)}
+          >
+            High
+          </Button>
+          <Button
+            variant={
+              filter.priority === PRIORITY.MEDIUM && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.MEDIUM)}
+          >
+            Medium
+          </Button>
+          <Button
+            variant={
+              filter.priority === PRIORITY.LOW && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.LOW)}
+          >
+            Low
+          </Button>
+          <Button
+            variant="light"
+            color={"indigo.4"}
+            size="sm"
+            rightIcon={<IconX size={"1rem"} />}
+            onClick={() => setFilter("")}
+          >
+            Clear Filter
+          </Button>
+        </Button.Group>
+      </MediaQuery>
+
+      <MediaQuery largerThan={"md"} styles={{ display: "none" }}>
+        <Button.Group>
+          <Button
+            variant={
+              filter.priority === PRIORITY.HIGH && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.HIGH)}
+            size="xs"
+          >
+            High
+          </Button>
+          <Button
+            variant={
+              filter.priority === PRIORITY.MEDIUM && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.MEDIUM)}
+            size="xs"
+          >
+            Medium
+          </Button>
+          <Button
+            variant={
+              filter.priority === PRIORITY.LOW && !filter.dueDate
+                ? "filled"
+                : "outline"
+            }
+            color={COLORS.PRIORITYBUTTON}
+            onClick={() => setFilter(PRIORITY.LOW)}
+            size="xs"
+          >
+            Low
+          </Button>
+          <Button
+            variant="light"
+            color={"indigo.4"}
+            size="xs"
+            rightIcon={<IconX size={"1rem"} />}
+            onClick={() => setFilter("")}
+          >
+            Clear
+          </Button>
+        </Button.Group>
+      </MediaQuery>
     </>
   );
 };
