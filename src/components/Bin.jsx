bde0643e32ae6452189d519cce9b62d60de7c511
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-09-19 18:25:10 +0600

    Make Bin.jsx responsive
    - Slightly adjust margin

--- src/components/Bin.jsx
+++ src/components/Bin.jsx
@@ -3,27 +3,29 @@ import { IconTrashFilled } from "@tabler/icons-react";
 
 const Bin = ({ trash, handleRetrieveAllClick, handleEmptyBinClick }) => {
   return (
-    <Group position="right" my={"xl"}>
-      {trash.length !== 0 ? (
-        <Menu width={200} shadow="md" withArrow position="left" offset={0}>
-          <Menu.Target>
-            <ActionIcon size={"4rem"} color="red.4" variant="transparent">
-              <IconTrashFilled stroke="1px" size={"3rem"} />
-            </ActionIcon>
-          </Menu.Target>
+    <>
+      <Group position="right" my={"xs"}>
+        {trash.length !== 0 ? (
+          <Menu width={200} shadow="md" withArrow position="left" offset={0}>
+            <Menu.Target>
+              <ActionIcon size={"4rem"} color="red.4" variant="transparent">
+                <IconTrashFilled stroke="1px" size={"3rem"} />
+              </ActionIcon>
+            </Menu.Target>
 
-          <Menu.Dropdown>
-            <Menu.Item onClick={() => handleRetrieveAllClick()}>
-              Retrieve all
-            </Menu.Item>
+            <Menu.Dropdown>
+              <Menu.Item onClick={() => handleRetrieveAllClick()}>
+                Retrieve all
+              </Menu.Item>
 
-            <Menu.Item onClick={() => handleEmptyBinClick()}>
-              Empty Bin
-            </Menu.Item>
-          </Menu.Dropdown>
-        </Menu>
-      ) : null}
-    </Group>
+              <Menu.Item onClick={() => handleEmptyBinClick()}>
+                Empty Bin
+              </Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+        ) : null}
+      </Group>
+    </>
   );
 };
 
