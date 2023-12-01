5854d7c6a41536c461db0e406a66bf52f04276c1
Ishmam Iqbal <ishmam.iqbal@sazim.io>
2023-12-02 04:39:29 +0600

    Add test suite for RemoveCompletedConfirmModal component.
    - Add RemoveCompletedConfirmModal.test.js file.
    - Add render and button click unit tests.
    - No snapshot test as mantine generates new ids every render causing failed tests.

---
+++ src/pages/HomePage/Components/RemoveCompletedConfirmModal/RemoveCompletedConfirmModal.test.js
@@ -0,0 +1,74 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import RemoveCompletedConfirmModal from "./RemoveCompletedConfirmModal";
+import userEvent from "@testing-library/user-event";
+
+const mockClearAllCompletedTasks = jest.fn();
+const mockCloseModal = jest.fn();
+
+describe("RemoveCompletedConfirmModal", () => {
+  test("renders correctly", () => {
+    const { container } = render(
+      <RemoveCompletedConfirmModal
+        clearAllCompletedTasks={mockClearAllCompletedTasks}
+        closeModal={mockCloseModal}
+        isConfirmModalOpen
+      />
+    );
+
+    const titleElement = screen.getByRole("heading", {
+      name: /confirm delete/i,
+    });
+    const areYouSureElement = screen.getByText(
+      "Are you sure you want to permanently delete all completed tasks?"
+    );
+    const keepThemButton = screen.getByRole("button", {
+      name: "No, Keep them.",
+    });
+    const deleteButton = screen.getByRole("button", {
+      name: "Yes, Delete!",
+    });
+
+    expect(titleElement).toBeInTheDocument();
+    expect(areYouSureElement).toBeInTheDocument();
+    expect(keepThemButton).toBeInTheDocument();
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  test("calls closeModal function on 'No, Keep them.' button click", async () => {
+    render(
+      <RemoveCompletedConfirmModal
+        clearAllCompletedTasks={mockClearAllCompletedTasks}
+        closeModal={mockCloseModal}
+        isConfirmModalOpen
+      />
+    );
+
+    const keepThemButton = screen.getByRole("button", {
+      name: "No, Keep them.",
+    });
+
+    await userEvent.click(keepThemButton);
+
+    expect(mockCloseModal).toHaveBeenCalled();
+  });
+
+  test("calls clearAllCompletedTasks and closeModal functions on 'Yes, Delete!' button click", async () => {
+    render(
+      <RemoveCompletedConfirmModal
+        clearAllCompletedTasks={mockClearAllCompletedTasks}
+        closeModal={mockCloseModal}
+        isConfirmModalOpen
+      />
+    );
+
+    const deleteButton = screen.getByRole("button", {
+      name: "Yes, Delete!",
+    });
+
+    await userEvent.click(deleteButton);
+
+    expect(mockClearAllCompletedTasks).toHaveBeenCalled();
+    expect(mockCloseModal).toHaveBeenCalled();
+  });
+});
